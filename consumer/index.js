1f26196f6d37b3472bc3a791629f95de938f856b
Janttonen <bgu252@myy.haaga-helia.fi>
2023-03-26 20:10:10 +0300

    final commit

--- consumer/index.js
+++ consumer/index.js
@@ -1,28 +1,75 @@
-import { Kafka } from "kafkajs";
+import { Kafka, Partitioners } from "kafkajs";
+import { v4 as UUID } from "uuid";
 console.log("*** Consumer starts... ***");
 
 const kafka = new Kafka({
     clientId: 'checker-server',
     brokers: ['localhost:9092']
 });
+const producer = kafka.producer({ createPartitioner: Partitioners.DefaultPartitioner })
+const consumer = kafka.consumer({ groupId: 'kafka-checker-servers1' });
 
-const consumer = kafka.consumer({ groupId: 'kafka-checker-servers1'});
+//check sent id
+function checkId(value) {
+    if (value.length == 11 && Number(value.substring(0, 1))) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 const run = async () => {
+    //Consumer
     await consumer.connect()
-    await consumer.subscribe({topic: 'tobechecked', fromBeginning: true })
+    await consumer.subscribe({ topic: 'tobechecked', fromBeginning: true })
 
     await consumer.run({
-eachMessage: async ({topic, partition, message}) => {
-    console.log({
-        key: message.key.toString(),
-        partition: message.partition,
-        offset: message.offset,
-        value: message.value.toString(),
-    })
-},
+        eachMessage: async ({ topic, partition, message }) => {
+            if (checkId(message.value.toString()) == true) {
+
+                const run = async () => {
+                    // Producing
+                    await producer.connect()
+
+                    setInterval(() => {
+                        queueIDMessage(message.value);
+                    }, 2500)
+                }
+                run().catch(console.error);
+
+                console.log({
+                    key: message.key.toString(),
+                    partition: message.partition,
+                    offset: message.offset,
+                    value: message.value.toString(),
+                })
+
+            }
+        },
 
     })
 }
 
 run().catch(console.error);
+
+async function queueIDMessage(value) {
+
+    const uuidFraction = UUID().substring(0, 4);
+    // Producer
+    const success = await producer.send({
+        topic: 'checkedresults',
+        messages: [
+            {
+                key: uuidFraction,
+                value: value + ' is checked',
+            },
+        ],
+    }
+    );
+
+    if (success) {
+        console.log(`Checked result message ${uuidFraction} succesfully to the stream`);
+    } else {
+        console.log('Problem writing to the stream...')
+    }
+}
--- producer/index.js
+++ producer/index.js
@@ -1,20 +1,40 @@
 import { Kafka, Partitioners } from "kafkajs";
 import { v4 as UUID } from "uuid";
-console.log("*** Consumer starts... ***");
+console.log("*** Producer starts... ***");
 
 const kafka = new Kafka({
     clientId: 'my-checking-client',
     brokers: ['localhost:9092']
 });
 
-const producer = kafka.producer({ createPartitioner: Partitioners.DefaultPartitioner})
-const run = async () => {
-    // Producing
-    await producer.connect()
+const producer = kafka.producer({ createPartitioner: Partitioners.DefaultPartitioner })
+const consumer = kafka.consumer({ groupId: 'kafka-checker-servers1' });
 
-    setInterval( () => {
+const run = async () => {
+    //Consumer
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'checkedresults', fromBeginning: false })
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+
+            console.log({
+                key: message.key.toString(),
+                partition: message.partition,
+                offset: message.offset,
+                value: message.value.toString(),
+            })
+
+        }
+    },
+
+    )
+
+    await producer.connect()
+    setInterval(() => {
         queueMessage();
     }, 2500)
+
 }
 run().catch(console.error);
 
@@ -29,25 +49,26 @@ const idNumbers = [
 ]
 
 function randomizeIntegerBetween(from, to) {
-    return (Math.floor(Math.random() * (to-from+1))) + from;
+    return (Math.floor(Math.random() * (to - from + 1))) + from;
 }
 
 async function queueMessage() {
 
-    const uuidFraction = UUID().substring(0,4);
+    const uuidFraction = UUID().substring(0, 4);
 
+    // Producer
     const success = await producer.send({
         topic: 'tobechecked',
         messages: [
             {
                 key: uuidFraction,
-                value: Buffer.from(idNumbers[randomizeIntegerBetween(0, idNumbers.length-1)]),
+                value: Buffer.from(idNumbers[randomizeIntegerBetween(0, idNumbers.length - 1)]),
             },
         ],
     }
     );
 
-    if(success) {
+    if (success) {
         console.log(`Message ${uuidFraction} succesfully to the stream`);
     } else {
         console.log('Problem writing to the stream...')
